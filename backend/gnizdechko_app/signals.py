32041c01b2e18d0c992f3cb393639d58cd4d6c57
Kateryna <katya.stelmakhova@gmail.com>
2026-02-03 17:51:33 +0100

    fix deploy

--- backend/gnizdechko_app/signals.py
+++ backend/gnizdechko_app/signals.py
@@ -1,9 +1,6 @@
 # gnizdechko_app/signals.py
-import os
-from django.db.models.signals import post_migrate
 from django.db.models.signals import post_delete, pre_save
 from django.dispatch import receiver
-from django.contrib.auth import get_user_model
 
 from .models import Product, ProductImage
 
