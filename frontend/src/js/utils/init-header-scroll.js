e3e483315465a6167fe4c6e38aa27b2c08116bd5
Kateryna <katya.stelmakhova@gmail.com>
2026-02-03 16:58:01 +0100

    header fix

--- frontend/src/js/utils/init-header-scroll.js
+++ frontend/src/js/utils/init-header-scroll.js
@@ -2,7 +2,7 @@ export const initHeaderScroll = () => {
   const header = document.getElementById('header');
   if (!header) return;
 
-  const OFFSET = 120;
+  const OFFSET = 80;
 
   const onScroll = () => {
     header.classList.toggle('header--active', window.scrollY >= OFFSET);
